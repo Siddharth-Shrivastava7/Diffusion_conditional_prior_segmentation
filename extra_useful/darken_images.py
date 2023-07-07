8045593ff75e51c70b63786d64b69d27139aad1f
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-07-07 14:38:30 +0530

    update

--- extra_useful/darken_images.py
+++ extra_useful/darken_images.py
@@ -6,20 +6,23 @@ from tqdm import tqdm
 # Function to rename multiple files
 def main():
    
-    folder = "/home/sidd_s/scratch/dataset/cityscapes/leftImg8bit/val"  
-    factor = 0.5
+    folder = "/home/sidd_s/scratch/dataset/cityscapes/leftImg8bit/train"  
+    factor = 0.3
     
     for root, dirs, files in os.walk(folder, topdown=False):
         for name in tqdm(files): 
             path = os.path.join(root, name) 
-            new_folder = root.replace('/leftImg8bit/', '/leftImg8bit_darken_brightness' + str(factor) +  '/') 
+            new_folder = root.replace('/leftImg8bit/', '/leftImg8bit_darken_contrast_' + str(factor) +  '/') 
             if not os.path.exists(new_folder):
                 os.makedirs(new_folder)
             img = Image.open(path)
             
             dst = f"{new_folder}/{name}" 
-            brightness = ImageEnhance.Brightness(img)
-            brightness.enhance(factor).save(dst) 
+            # brightness = ImageEnhance.Brightness(img)
+            # brightness.enhance(factor).save(dst) 
+            contrast = ImageEnhance.Contrast(img)
+            contrast.enhance(factor).save(dst)
+            # break 
         
 # Driver Code
 if __name__ == '__main__':
