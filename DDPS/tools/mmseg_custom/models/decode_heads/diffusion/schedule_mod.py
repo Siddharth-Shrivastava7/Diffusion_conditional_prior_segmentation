d8d08cbdc839e732ed5022080c19da7ea47d858a
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-08-14 13:12:20 +0530

    expo spacing added

--- DDPS/tools/mmseg_custom/models/decode_heads/diffusion/schedule_mod.py
+++ DDPS/tools/mmseg_custom/models/decode_heads/diffusion/schedule_mod.py
@@ -70,9 +70,10 @@ def cos_alpha_schedule_torch(time_step, N=100, att_1=0.99999, att_T=0.000009, ct
 
 ## custom beta_schedule  (linear) 
 def custom_schedule(beta_start = 0.0001, beta_end = 0.02, timesteps=20,dtype=torch.float64):
-    betas = torch.linspace(beta_start, beta_end, timesteps, dtype=dtype)
+    # betas = torch.linspace(beta_start, beta_end, timesteps, dtype=dtype)
     # betas = -torch.log(torch.linspace(beta_start, beta_end, timesteps, dtype=dtype))
-    # betas = torch.linspace(beta_start, beta_end, timesteps, dtype=dtype)**2 ## quadratic
+    # betas = torch.linspace(beta_start, beta_end, timesteps, dtype=dtype)**2 ## quadratic 
+    betas = torch.logspace(-5.5, -4.5, base = 10, num=20) ## expo space growth...increases slowly in the start and raipdy grows in the end! ## hyperparam tuned in such a way that classes in the dia confuses with the off dia classes 
     return betas 
 
 def q_pred(x_start, t, num_timesteps, num_classes, log_cumprod_at, log_cumprod_bt):           # q(xt|x0)
