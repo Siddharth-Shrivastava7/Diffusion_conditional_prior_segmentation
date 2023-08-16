d362f66e845510810823716719e4ced97823c425
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-08-16 13:00:07 +0530

    push

--- DDP/segmentation/mmseg/models/segmentors/ssd.py
+++ DDP/segmentation/mmseg/models/segmentors/ssd.py
@@ -18,6 +18,8 @@ import os
 from ..builder import SEGMENTORS
 from .encoder_decoder import EncoderDecoder
 
+from ..discrete_diffusion.schedule_mod import q_mats_from_onestepsdot, q_pred_from_mats
+
 def log(t, eps=1e-20):
     return torch.log(t.clamp(min=eps))
 
@@ -43,8 +45,6 @@ def log_snr_to_alpha_sigma(log_snr):
 '''
 
 
-
-
 @SEGMENTORS.register_module()
 class SSD(EncoderDecoder):
     """Encoder Decoder segmentors.
@@ -53,36 +53,39 @@ class SSD(EncoderDecoder):
     which could be dumped during inference.
     """
     def __init__(self,
-                 bit_scale=0.1,
-                 timesteps=1,
-                 randsteps=1,
-                 time_difference=1,
-                 learned_sinusoidal_dim=16,
-                 sample_range=(0, 0.999),
-                 noise_schedule='cosine',
-                 diffusion='ddim',
-                 accumulation=False,
-                 **kwargs):
+                timesteps = 20, 
+                **kwargs):
         super(SSD, self).__init__(**kwargs)
-
-        self.bit_scale = bit_scale
-        self.timesteps = timesteps
-        self.randsteps = randsteps
-        self.diffusion = diffusion
-        self.time_difference = time_difference
-        self.sample_range = sample_range
-        self.use_gt = False
-        self.accumulation = accumulation
-        self.embedding_table = nn.Embedding(self.num_classes + 1, self.decode_head.in_channels[0])
-
-        print(f" timesteps: {timesteps},"
-              f" randsteps: {randsteps},"
-              f" sample_range: {sample_range},"
-              f" diffusion: {diffusion}")
-
-        if noise_schedule == "linear":
-            self.log_snr = beta_linear_log_snr
-        elif noise_schedule == "cosine":
-            self.log_snr = alpha_cosine_log_snr
-        else:
-            raise ValueError(f'invalid noise schedule {noise_schedule}')
+        
+        self.timesteps = timesteps 
+        self.embedding_table = nn.Embedding(self.num_classes + 1, self.num_classes + 1) # instead of one hot encoding making class embedding module for discrete data space
+        self.transform = ConvModule(
+            self.decode_head.in_channels[0] + (self.num_classes + 1),
+            self.decode_head.in_channels[0],
+            1,
+            padding=0,
+            conv_cfg=None,
+            norm_cfg=None,
+            act_cfg=None
+        ) # used for converting concatenated encoded i/p image and encoded;corrupted gt map to feature maps of dimension being half of the joint dimension of the concatenated inputs
+        
+        
+        
+    def forward_train(self, img, img_metas, gt_semantic_seg):
+        """Forward function for training.
+        Args:
+            img (Tensor): Input images.
+            img_metas (list[dict]): List of image info dict where each dict
+                has: 'img_shape', 'scale_factor', 'flip', and may also contain
+                'filename', 'ori_shape', 'pad_shape', and 'img_norm_cfg'.
+                For details on the values of these keys see
+                `mmseg/datasets/pipelines/formatting.py:Collect`.
+            gt_semantic_seg (Tensor): Semantic segmentation masks
+                used if the architecture supports semantic segmentation task.
+        Returns:
+            dict[str, Tensor]: a dictionary of loss components
+        """
+        x = self.extract_feat(img)[0]  # bs, 256, h/4, w/4
+        batch, c, h, w, device, = *x.shape, x.device
+    
+    
