418130520e884359f6767e7d20d32435b979bf5c
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-08-26 12:23:04 +0530

    things which can also be done in ssd

---
+++ extra_useful/things_to_do_in_ssd.py
@@ -0,0 +1,20 @@
+
+''' 
+
+    What all things we can do in ssd, more to improve it further? 
+    
+    
+    
+>> on label embedding
+    1.Change the label embedding dim, as acc to DDP 
+    2.Change the current embedding to one hot vector embedding 
+>> On time dimension
+    1. Change it to 16, as acc to DDP 
+>> Number of schedule steps 
+    1. Change it to 10 steps
+    2. Change it to 5 steps 
+    # Can check at each step improvement in terms of miou
+>> mutual information based noise scheduling type
+    1. change it to cosine 
+>> 
+'''
