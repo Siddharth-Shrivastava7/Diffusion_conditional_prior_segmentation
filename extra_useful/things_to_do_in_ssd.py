4639f02f45d3327f5cba9f6901ddbc973c132d17
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-09-21 19:13:35 +0530

    MI based linear cosine scheule

--- extra_useful/things_to_do_in_ssd.py
+++ extra_useful/things_to_do_in_ssd.py
@@ -104,7 +104,7 @@ time step will remain 3 only
 
 '''
 to make grid of latent state space of diffusion models of different noise schedules: 
-    1. MI based linear schedule 
-    2. MI based cosine schedule 
+    1. MI based linear schedule  ## not performing so well
+    2. MI based cosine schedule  ## not performing so well 
     3. constant beta schedule : (1-beta_t)*I + beta_t*(normalised_confusion_matrix)    
 '''
