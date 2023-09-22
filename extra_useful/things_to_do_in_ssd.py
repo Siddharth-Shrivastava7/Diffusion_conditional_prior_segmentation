d9ec1383b5c754de27a4ef03a9d16fec41b2c86d
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-09-22 08:33:55 +0530

    Instead of gumbel argmax sampling, directly...trying with directly argmax sampling

--- extra_useful/things_to_do_in_ssd.py
+++ extra_useful/things_to_do_in_ssd.py
@@ -108,5 +108,8 @@ to make grid of latent state space of diffusion models of different noise schedu
     2. MI based cosine schedule  ## not performing so well 
     3. constant beta schedule : (1-beta_t)*I + beta_t*(normalised_confusion_matrix)   ## done, this also showing results similar to above two points
     
-Next could be linear/cosine scheduling with beta schedule; where transition matrix is of form similar to constant beta schedule transition matrix. 
+Next could be linear/cosine scheduling with beta schedule; where transition matrix is of form similar to constant beta schedule transition matrix.  
+
+Instead of gumbel argmax sampling, directly...trying with directly sampling, not introducing more noise...lets see and experiment...
+
 '''
