7699a0d13992b766a17093ead9658b93aff1cc12
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-10-08 12:07:33 +0530

    restructing

--- IADB/iadb.py
+++ IADB/iadb.py
@@ -37,43 +37,49 @@ def sample_iadb(model, x0, nb_step):
 
     return x_alpha
 
+def main(): 
+    print('in the main function')
+    device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+    CELEBA_FOLDER = '/home/sidd_s/scratch/dataset/celeba/img_align_celeba/'
+    transform = transforms.Compose([transforms.Resize(64),transforms.CenterCrop(64), transforms.RandomHorizontalFlip(0.5),transforms.ToTensor()])
+    train_dataset = torchvision.datasets.CelebA(root=CELEBA_FOLDER, split='train',
+                                            download=True, transform=transform)
 
-device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
-CELEBA_FOLDER = '/home/sidd_s/scratch/dataset/celeba/img_align_celeba/'
-transform = transforms.Compose([transforms.Resize(64),transforms.CenterCrop(64), transforms.RandomHorizontalFlip(0.5),transforms.ToTensor()])
-train_dataset = torchvision.datasets.CelebA(root=CELEBA_FOLDER, split='train',
-                                        download=True, transform=transform)
+    dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=0, drop_last=True) 
+    print('dataset loaded successfully!')
 
-dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=0, drop_last=True)
+    model = get_model() 
+    model = model.to(device)
+    print('Model loaded into cuda')
 
-model = get_model() 
-model = model.to(device)
-print('Model loaded into cuda')
+    optimizer = Adam(model.parameters(), lr=1e-4)
+    nb_iter = 0
+    print('Start training')
+    for current_epoch in tqdm(range(100)):
+        for i, data in enumerate(dataloader):
+            x1 = (data[0].to(device)*2)-1
+            x0 = torch.randn_like(x1)
+            bs = x0.shape[0]
 
-optimizer = Adam(model.parameters(), lr=1e-4)
-nb_iter = 0
-print('Start training')
-for current_epoch in tqdm(range(100)):
-    for i, data in enumerate(dataloader):
-        x1 = (data[0].to(device)*2)-1
-        x0 = torch.randn_like(x1)
-        bs = x0.shape[0]
+            alpha = torch.rand(bs, device=device)
+            x_alpha = alpha.view(-1,1,1,1) * x1 + (1-alpha).view(-1,1,1,1) * x0
+            
+            d = model(x_alpha, alpha)['sample']
+            loss = torch.sum((d - (x1-x0))**2)
 
-        alpha = torch.rand(bs, device=device)
-        x_alpha = alpha.view(-1,1,1,1) * x1 + (1-alpha).view(-1,1,1,1) * x0
-        
-        d = model(x_alpha, alpha)['sample']
-        loss = torch.sum((d - (x1-x0))**2)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            nb_iter += 1
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        nb_iter += 1
+            if nb_iter % 200 == 0: 
+                print('In Sampling')
+                with torch.no_grad():
+                    print(f'Save export {nb_iter}')
+                    sample = (sample_iadb(model, x0, nb_step=128) * 0.5) + 0.5
+                    torchvision.utils.save_image(sample, f'/home/sidd_s/scratch/saved_models/iadb/export_{str(nb_iter).zfill(8)}.png')
+                    torch.save(model.state_dict(), f'/home/sidd_s/scratch/saved_models/iadb/celeba.ckpt')
 
-        if nb_iter % 200 == 0: 
-            print('In Sampling')
-            with torch.no_grad():
-                print(f'Save export {nb_iter}')
-                sample = (sample_iadb(model, x0, nb_step=128) * 0.5) + 0.5
-                torchvision.utils.save_image(sample, f'/home/sidd_s/scratch/saved_models/iadb/export_{str(nb_iter).zfill(8)}.png')
-                torch.save(model.state_dict(), f'/home/sidd_s/scratch/saved_models/iadb/celeba.ckpt')
+
+if __name__ == '__main__':
+    main()
