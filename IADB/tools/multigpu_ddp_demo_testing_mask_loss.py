e36202008b212250f8133ad1e9e53c9ddaedee7f
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-10-26 12:55:23 +0530

    testing with lower dimension, for checking its working

--- IADB/tools/multigpu_ddp_demo_testing_mask_loss.py
+++ IADB/tools/multigpu_ddp_demo_testing_mask_loss.py
@@ -367,7 +367,7 @@ def main(rank: int, world_size: int, save_every: int, total_epochs: int, nb_step
 if __name__ == '__main__':
     to_correct_model_path = '/home/guest/scratch/siddharth/data/saved_models/mmseg/segformer_b2_cityscapes_1024x1024/segformer_mit-b2_8x1_1024x1024_160k_cityscapes_20211207_134205-6096669a.pth'
     to_correct_config_path = '/home/guest/scratch/siddharth/data/saved_models/mmseg/segformer_b2_cityscapes_1024x1024/segformer_mit-b2_8xb1-160k_cityscapes-1024x1024.py'
-    resize_shape = (512, 1024)
+    resize_shape = (256, 512) ## testing with lower dimension, for checking its working
     save_every = 25
     total_epochs = 860 ## similar to DDP 160k iter @ batch size 16
     nb_steps = 128 ## similar to IADB 
